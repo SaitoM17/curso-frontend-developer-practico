eb95ea63026718adcb01d4ed6d6bddbae01c048e
Said Mariano <smariano170@gmail.com>
2022-12-30 11:30:11 -0600

    Fusión del menú en mobile

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click',toggleDesktopMenu);
+menuHamIcon.addEventListener('click',toggleMobileMenu);
+// menuHamIcon.addEventListener('click', () => {
+//     mobileMenu.classList.toggle('inactive');
+// });
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
