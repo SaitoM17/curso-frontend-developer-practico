ea15e145f4fed2fe43ef1cd20526b840ccd67370
Said Mariano <smariano170@gmail.com>
2022-12-30 15:46:24 -0600

    A gregamos Lista de productos

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardContainer = document.querySelector('.cards-container');
 
 navEmail.addEventListener('click',toggleDesktopMenu);
 menuHamIcon.addEventListener('click',toggleMobileMenu);
@@ -42,3 +43,70 @@ function toggleCarritoAside(){
     aside.classList.toggle('inactive');
 
 }
+
+const productList = [];
+
+productList.push({
+    name: 'Bike1',
+    price: 120,
+    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+
+productList.push({
+    name: 'Bike2',
+    price: 120,
+    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+
+productList.push({
+    name: 'Bike3',
+    price: 120,
+    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+
+productList.push({
+    name: 'Bike4',
+    price: 120,
+    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+
+function renderProduct(arr){
+    for(product of productList){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.imagen);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productdiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = `$` + product.price;
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productdiv.appendChild(productPrice);
+        productdiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img')
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg' )
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productdiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardContainer.appendChild(productCard);
+    
+    }
+}
+
+renderProduct(productList);
